7364f1c0289c6372f690a589c39b2b091de1994b
Jawad <bawskiya@gmail.com>
2024-01-19 17:06:53 -0600

    💄 add border to TabsList in TopGenres component

--- components/top-genres.tsx
+++ components/top-genres.tsx
@@ -58,7 +58,7 @@ const TopGenres = ({ className, ...props }: ComponentProps<"div">) => {
     <div {...props} className={twMerge(className)}>
       <UserSection label="Top Genres" />
       <Tabs defaultValue="short_term" className="mt-4">
-        <TabsList className="w-full md:w-1/2">
+        <TabsList className="w-full border border-black/10 dark:border-white/10 md:w-1/2">
           <TabsTrigger
             onClick={() => setTimeframe("short_term")}
             value="short_term"
