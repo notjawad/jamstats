fa4a6bc0038336816f33b10c48b04874094dc29f
Jawad <bawskiya@gmail.com>
2024-01-19 17:07:33 -0600

    💄 refactor user section component styling

--- components/user-section.tsx
+++ components/user-section.tsx
@@ -12,14 +12,17 @@ const UserSection = ({ className, label, ...props }: UserSectionProps) => {
   const { data: session, status } = useSession();
 
   return (
-    <div {...props} className="flex items-center justify-between">
+    <div
+      {...props}
+      className="flex items-center justify-between rounded-md border border-black/10 bg-accent p-2 dark:border-white/10"
+    >
       <div className="flex items-center">
         <Image
           src={session?.user?.image || ""}
           alt="User Image"
           width={64}
           height={64}
-          className="rounded-full shadow-md"
+          className="rounded-md shadow-md"
         />
         <div className="ml-4 truncate">
           <h2 className="truncate text-lg font-medium leading-6">
