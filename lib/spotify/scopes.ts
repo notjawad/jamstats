249b3426119d5572572a6f3a7d50f14374175690
Jawad <bawskiya@gmail.com>
2024-01-19 13:09:48 -0600

    chore: update Spotify scopes

--- lib/spotify/scopes.ts
+++ lib/spotify/scopes.ts
@@ -1,2 +1,2 @@
 export const scopes =
-  "user-read-email playlist-read-private user-top-read playlist-read-collaborative playlist-modify-private playlist-modify-public user-library-read user-library-modify";
+  "user-read-email user-read-currently-playing playlist-read-private user-top-read playlist-read-collaborative playlist-modify-private playlist-modify-public user-library-read user-library-modify";
