f6a481997869712c758e43b7f9697b834012ecb5
Jawad <bawskiya@gmail.com>
2024-01-19 21:23:50 -0600

    ✨ add new utility functions to lib/utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,27 @@ export function removeHtmlTags(str: string | undefined): string {
   if (!str) return "";
   return str.replace(/(<([^>]+)>)/gi, "");
 }
+
+export function toTitleCase(str: string | undefined): string {
+  if (!str) return "";
+
+  return str.replace(
+    /\w\S*/g,
+    (txt) => txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase(),
+  );
+}
+
+export function getSimilarTags(
+  tags: string[],
+  tag: string | undefined,
+): string[] {
+  if (!tag) return [];
+
+  const similarTags: string[] = [];
+  tags.forEach((t) => {
+    if (t.includes(tag)) {
+      similarTags.push(t);
+    }
+  });
+  return similarTags;
+}
