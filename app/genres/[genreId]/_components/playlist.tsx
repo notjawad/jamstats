f978a4e123a3b2d09b53c9ca6f08d83dad30a45a
Jawad <bawskiya@gmail.com>
2024-01-20 02:50:40 -0600

    Add Playlist component for genre

---
+++ app/genres/[genreId]/_components/playlist.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React, { ComponentProps, useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+
+import { twMerge } from "tailwind-merge";
+
+import axios from "axios";
+
+interface PlaylistProps extends ComponentProps<"div"> {
+  genre: string;
+}
+
+const Playlist = ({ genre, className, ...props }: PlaylistProps) => {
+  const { data: session } = useSession();
+
+  const [playlist, setPlaylist] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (session && "access_token" in session) {
+      const fetchPlaylist = async () => {
+        console.log("fetching playlist", decodeURIComponent(genre));
+        try {
+          const response = await axios.get(
+            "https://api.spotify.com/v1/search",
+            {
+              params: {
+                q: `The Sound of ${decodeURIComponent(genre)}`,
+                type: "playlist",
+              },
+              headers: {
+                Authorization: `Bearer ${session.access_token}`,
+              },
+            },
+          );
+
+          const playlists = response.data.playlists.items;
+          const filteredPlaylists = playlists.filter(
+            (playlist: { owner: { id: string } }) =>
+              playlist.owner.id.includes("thesoundsofspotify"),
+          );
+
+          setPlaylist(filteredPlaylists);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      fetchPlaylist();
+    }
+  }, [session]);
+
+  return (
+    <div className={twMerge(className)} {...props}>
+      {playlist.length > 0 && (
+        <>
+          <h1 className="text-2xl font-bold">Playlist</h1>
+          <p className="mb-4 flex items-center gap-x-1 text-sm font-medium text-muted-foreground">
+            Here is a playlist to get you started with{" "}
+            <span className="text-green-500">{decodeURIComponent(genre)}</span>{" "}
+            😀
+          </p>
+          <h2 className="mb-4 text-xl font-bold">
+            {/* Spotify embed */}
+            <div className="relative overflow-hidden rounded-xl border border-black/10 bg-accent p-2 dark:border-white/10">
+              <iframe
+                className="rounded-xl"
+                src={`https://open.spotify.com/embed/playlist/${playlist[0]?.id}`}
+                width="100%"
+                height="380"
+                allow="encrypted-media"
+              ></iframe>
+            </div>
+          </h2>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Playlist;
