2e40acf7f28a01dd739368bd80c284747ceb3e16
Jawad <bawskiya@gmail.com>
2024-01-20 00:54:13 -0600

    Remove unnecessary import statement in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { SessionProvider } from "next-auth/react";
-
 import Navbar from "@/components/navbar";
 
 type RootLayoutProps = {
