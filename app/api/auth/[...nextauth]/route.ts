8da061ceabd9ddc8e2cf122f8abd2074ead45bfd
Jawad <bawskiya@gmail.com>
2024-01-20 15:49:46 -0600

    Refactor Spotify token refresh logic

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,53 +2,17 @@ import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
 import { scopes } from "@/lib/spotify/scopes";
-import axios from "axios";
-import { JWT } from "next-auth/jwt";
 
-const SPOTIFY_REFRESH_TOKEN_URL = "https://accounts.spotify.com/api/token";
-const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-
-async function refreshAccessToken(token: JWT): Promise<JWT> {
-  try {
-    if (token.accessTokenExpires ?? 0 > Date.now()) {
-      return token;
-    }
-
-    const basicAuth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
-      "base64",
-    );
-    const { data } = await axios.post(
-      SPOTIFY_REFRESH_TOKEN_URL,
-      {
-        grant_type: "refresh_token",
-        refresh_token: token.refreshToken,
-      },
-      {
-        headers: {
-          Authorization: `Basic ${basicAuth}`,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      },
-    );
-    return {
-      ...token,
-      accessToken: data.access_token,
-      accessTokenExpires: Date.now() + data.expires_in * 1000,
-    };
-  } catch (error) {
-    return {
-      ...token,
-      error: "RefreshAccessTokenError",
-    };
-  }
-}
+const isTokenExpired = (token: { expires: number }) => {
+  const currentTime = Math.floor(Date.now() / 1000);
+  return token.expires < currentTime;
+};
 
 const handler = NextAuth({
   providers: [
     SpotifyProvider({
-      clientId: CLIENT_ID as string,
-      clientSecret: CLIENT_SECRET as string,
+      clientId: process.env.SPOTIFY_CLIENT_ID as string,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
       authorization: {
         params: {
           scope: scopes,
@@ -63,23 +27,49 @@ const handler = NextAuth({
   callbacks: {
     async jwt({ token, account }) {
       if (account) {
-        token = Object.assign(token, {
+        token = Object.assign({}, token, {
           access_token: account.access_token,
           refresh_token: account.refresh_token,
-          expires: (account.expires_at ?? 0) * 1000,
+          expires: account.expires_at,
         });
       }
 
-      token = await refreshAccessToken(token);
+      if (isTokenExpired(token as { expires: number })) {
+        const url = new URL("https://accounts.spotify.com/api/token");
+        const body = new URLSearchParams({
+          grant_type: "refresh_token",
+          refresh_token: token.refresh_token as string,
+        });
 
-      return token;
+        const response = await fetch(url.toString(), {
+          method: "POST",
+          headers: {
+            Authorization: `Basic ${Buffer.from(
+              `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`,
+            ).toString("base64")}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: body.toString(),
+        });
+
+        const refreshedToken = await response.json();
+
+        token = Object.assign({}, token, {
+          access_token: refreshedToken.access_token,
+          refresh_token: refreshedToken.refresh_token,
+          expires: refreshedToken.expires_in,
+        });
+        return token;
+      } else {
+        return token;
+      }
     },
 
     async session({ session, token }) {
       if (session) {
         session = Object.assign({}, session, {
-          id: token.sub,
           access_token: token.access_token,
+          refresh_token: token.refresh_token,
         });
       }
       return session;
