82166b24b0fa1fb2d18eea1a2e3196c091db9e2e
Jawad <bawskiya@gmail.com>
2024-01-19 20:47:34 -0600

    ✨ add Genre page

---
+++ app/genres/[genreId]/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { Tag } from "@/lib/spotify/types";
+import { Spinner } from "@/components/spinner";
+import axios from "axios";
+import { removeHtmlTags } from "@/lib/utils";
+import Artists from "./_components/artists";
+
+const GenrePage = () => {
+  const params = useParams<{ genreId: string }>();
+  const { genreId } = params;
+
+  const [genreInfo, setGenreInfo] = useState<Tag>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchGenreInfo = async () => {
+      try {
+        const { data } = await axios.get(`/api/tag?tag=${genreId}`);
+        setGenreInfo(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchGenreInfo();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <div className="mx-auto w-screen max-w-3xl px-4 sm:px-6 md:px-12">
+      <div className="container">
+        <div className="mx-auto">
+          <div className="pb-10">
+            <div>
+              <h1 className="mb-2 inline-block text-2xl font-extrabold tracking-tight">
+                {genreInfo?.tag.name}
+              </h1>
+              <p className="mb-12 line-clamp-6 w-full text-sm text-muted-foreground xl:mr-64">
+                {removeHtmlTags(genreInfo?.tag.wiki.content) ||
+                  "No description available"}
+              </p>
+            </div>
+            <Artists tag={genreInfo?.tag.name} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GenrePage;
