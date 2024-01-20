f883426a7cdbe002a5a2abbd98b9e03811503551
Jawad <bawskiya@gmail.com>
2024-01-20 00:55:22 -0600

    Add loading state to UserSection component

--- components/user-section.tsx
+++ components/user-section.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { ComponentProps } from "react";
 import { useSession } from "next-auth/react";
 
@@ -10,6 +12,24 @@ interface UserSectionProps extends ComponentProps<"div"> {
 
 const UserSection = ({ className, label, ...props }: UserSectionProps) => {
   const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <div
+        {...props}
+        className="flex items-center justify-between rounded-md border border-black/10 bg-accent p-2 dark:border-white/10"
+      >
+        <div className="flex items-center">
+          <div className="h-16 w-16 animate-pulse rounded-md bg-black/10" />
+          <div className="ml-4">
+            <div className="h-4 w-32 animate-pulse rounded-md bg-black/10" />
+            <div className="mt-1 h-3 w-24 animate-pulse rounded-md bg-black/10" />
+          </div>
+        </div>
+        <div className="h-4 w-24 animate-pulse rounded-md bg-black/10" />
+      </div>
+    );
+  }
 
   return (
     <div
