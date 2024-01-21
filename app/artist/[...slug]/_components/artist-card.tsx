9b53c2d57e4c1f9c930fe849f4a796871c0f0215
Jawad <bawskiya@gmail.com>
2024-01-21 12:18:38 -0600

    Refactor artist card component and add Spotify button

--- app/artist/[...slug]/_components/artist-card.tsx
+++ app/artist/[...slug]/_components/artist-card.tsx
@@ -1,16 +1,25 @@
+"use client";
+
+import React, { ComponentProps } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import { twMerge } from "tailwind-merge";
+
 import { Artist } from "@/interfaces";
 import { Verified } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import React, { ComponentProps } from "react";
-import { BsInstagram, BsTwitterX } from "react-icons/bs";
-import { twMerge } from "tailwind-merge";
+import { BsInstagram, BsSpotify, BsTwitterX } from "react-icons/bs";
+import { useParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 interface ArtistCardProps extends ComponentProps<"div"> {
   artist: Artist | null;
 }
 
 const ArtistCard = ({ artist, className, ...props }: ArtistCardProps) => {
+  const { slug } = useParams();
+  const artistId = slug[0];
+
   return (
     <div {...props} className={twMerge("rounded-md border p-4", className)}>
       <div className="flex items-center gap-x-4">
@@ -25,27 +34,29 @@ const ArtistCard = ({ artist, className, ...props }: ArtistCardProps) => {
             image.currentTarget.classList.remove("opacity-0");
           }}
         />
-        <div>
-          <h2 className="mt-4 flex items-center text-xl font-bold">
-            {artist?.name}
-            {artist?.is_verified && (
-              <span className="ml-2 text-blue-500">
-                <Verified className="h-4 w-4" />
-              </span>
-            )}
-          </h2>
+        <div className="flex w-full flex-col">
+          <div className="mt-4 flex items-center justify-between text-xl font-bold">
+            <h2 className="flex items-center">
+              {artist?.name}
+              {artist?.is_verified && (
+                <span className="ml-2 text-blue-500">
+                  <Verified className="h-4 w-4" />
+                </span>
+              )}
+            </h2>
+            {/* Move the Spotify button outside the previous container */}
+            <Link href={`https://open.spotify.com/artist/${artistId}`}>
+              <Button
+                className="flex items-center gap-x-2 text-xs"
+                variant="outline"
+                size="sm"
+              >
+                <BsSpotify className="h-3 w-3" />
+                Open in Spotify
+              </Button>
+            </Link>
+          </div>
           <div className="flex items-center gap-x-2">
-            {artist?.instagram_name && (
-              <p className="flex items-center gap-x-1 text-sm text-muted-foreground">
-                <BsInstagram className="h-3 w-3" />
-                <Link
-                  href={`https://instagram.com/${artist?.instagram_name}`}
-                  className="text-xs hover:underline"
-                >
-                  {artist?.instagram_name.toLowerCase()}
-                </Link>
-              </p>
-            )}
             {artist?.twitter_name && (
               <p className="flex items-center gap-x-1 text-sm text-muted-foreground">
                 <BsTwitterX className="h-3 w-3" />
@@ -54,6 +65,17 @@ const ArtistCard = ({ artist, className, ...props }: ArtistCardProps) => {
                   className="text-xs hover:underline"
                 >
                   {artist?.twitter_name.toLowerCase()}
+                </Link>
+              </p>
+            )}
+            {artist?.instagram_name && (
+              <p className="flex items-center gap-x-1 text-sm text-muted-foreground">
+                <BsInstagram className="h-3 w-3" />
+                <Link
+                  href={`https://instagram.com/${artist?.instagram_name}`}
+                  className="text-xs hover:underline"
+                >
+                  {artist?.instagram_name.toLowerCase()}
                 </Link>
               </p>
             )}
