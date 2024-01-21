bc791d42068a24a12ba77019a988547fa9582619
Jawad <bawskiya@gmail.com>
2024-01-21 01:24:48 -0600

    Add SpotifyTrack, SpotifyAlbum, and ArtistElement interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -222,3 +222,60 @@ export interface Artist {
     };
   };
 }
+
+export interface SpotifyTrack {
+  album: SpotifyAlbum;
+  artists: ArtistElement[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: {
+    isrc: string;
+  };
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  is_local: boolean;
+  is_playable: boolean;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+}
+
+export interface SpotifyAlbum {
+  album_type: string;
+  artists: ArtistElement[];
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  images: {
+    height: number;
+    url: string;
+    width: number;
+  }[];
+  is_playable: boolean;
+  name: string;
+  release_date: Date;
+  release_date_precision: string;
+  total_tracks: number;
+  type: string;
+  uri: string;
+}
+
+export interface ArtistElement {
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+}
