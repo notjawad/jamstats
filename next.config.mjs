44cee59a41f2cc64d12bd1ad13ca824a314fda0f
Jawad <bawskiya@gmail.com>
2024-01-22 02:30:50 -0600

    Update remotePatterns in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,19 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "i.scdn.co",
-      },
-      {
-        protocol: "https",
-        hostname: "images.genius.com",
-      },
-      {
-        protocol: "https",
-        hostname: "scontent-lga3-2.xx.fbcdn.net",
-      },
-      {
-        protocol: "https",
-        hostname: "platform-lookaside.fbsbx.com",
+        hostname: "**",
       },
     ],
   },
